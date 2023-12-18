16714f308af4263d942b47db3e69b6ff2fee420d
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-18 15:17:03 +0100

    feat(style): implement sass

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
+import './scss/index.scss'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
