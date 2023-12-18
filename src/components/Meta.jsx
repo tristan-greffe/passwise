a98bb975a430e3b1653f283d974b793269656df5
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-18 12:04:28 +0100

    feat(seo): implement react-helmet-async for dynamic meta tags

---
+++ src/components/Meta.jsx
@@ -0,0 +1,15 @@
+import { useTranslation } from 'react-i18next'
+import { Helmet } from 'react-helmet-async'
+
+function Meta ({ title, description }) {
+  const { t } = useTranslation()
+
+  return (
+    <Helmet>
+      <title>{ t(title) }</title>
+      <meta name="description" content={t(description)} />
+    </Helmet>
+  )
+}
+
+export default Meta
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
