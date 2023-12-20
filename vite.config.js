da82a2433d44c54366317efbe9fd1c5eb0e475b1
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-20 20:24:06 +0100

    chore: improve configuration to use process.env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr' 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    define: {
+      'process.env': env
+    },
+    plugins: [react(), svgr()],
+    server: {
+      host: true,
+      port: 8080
+    }
+  })
+}
