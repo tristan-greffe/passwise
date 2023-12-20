bc9a1dd9e1363aaabcbdb5e8bd7edb366ac1fd07
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-21 00:15:40 +0100

    chore: update layout session

--- src/components/LayoutSession.jsx
+++ src/components/LayoutSession.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import { Logo } from '../assets'
 import Form from './form/Form'
 
 function LayoutSession ({ data, fields, onSubmit, onFieldChanged }) {
@@ -9,10 +10,10 @@ function LayoutSession ({ data, fields, onSubmit, onFieldChanged }) {
     <div id={data.id}>
       <span>{ t(data.title)}</span>
       <div>
-        <h1>Espace <b>sécurisé</b></h1>
+        <Link to="/"><Logo /></Link>
         <div>
           <div className="locker">
-            <div className="locker_head"><div>{ t(data.title)}</div></div>
+            <h1 className="locker_head"><div>{ t(data.title)}</div></h1>
             <p className="text-center text-xs mb-6">{ t(data.message)}</p>
             <Form fields={fields} onSubmit={onSubmit} onFieldChanged={onFieldChanged} buttonLabel={data.buttonLabel} />
           </div>
