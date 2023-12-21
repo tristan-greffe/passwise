80976651fc892ae871767450ce2b2bb8fe7ea078
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-21 01:11:36 +0100

    chore: refine API initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Toast, Loader } from './components'
+import { setUser } from './store/userSlice'
+import { api, initializeApi } from './api'
+import config from './config.js'
 import { Home, Login, Signup, ForgotPassword, ResetPassword } from './pages'
 
 function App () {
+  const dispatch = useDispatch()
   const { loader } = useSelector(state => state.component)
   
+  initializeApi(config)
+
+  useEffect(() => {
+    const handleAuthentication = async () => {
+      try {
+        const response = await api.reAuthenticate()
+        const user = response.user ? response.user : {}
+        dispatch(setUser(user))
+      } catch (error) {
+        await api.logout()
+        await api.authentication.removeAccessToken()
+        dispatch(setUser({}))
+      }
+    }
+    handleAuthentication()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { Provider } from 'react-redux'
 import store from './store/index.js'
-import { initializeApi } from './api.js'
-import config from './config.js'
 import App from './App.jsx'
 import './scss/index.scss'
 import './i18n'
-
-initializeApi(config)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
